86d5c96f3236e8dd62ffeb9e6c79fec2b9477d03
Eduard Castellano <eduard@smartm.no>
2015-09-22 14:59:11 +0200

    clean up, docs

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # babel-plugin-stylus
+
+Replace import declarations of **Stylus** files to its parsed **CSS**:
+
+```js
+import style from './Component.styl'
+```
+
+to
+
+```js
+var style = '.Component {\n  font-family: \'Lucida Grande\';\n  color: #3d9970;\n  font-size: 24px;\n}\n';
+```
+
+
+## Install
+
+```
+npm i babel-plugin-stylus --save-dev
+```
+
+## Usage
+
+```
+babel --plugins babel-plugin-stylus src/ --out-dir lib
+```
+
+## Changelog
+
+### 1.0.0
+
+* First release :tada:
+
--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 'use strict';
 
+require('string.prototype.endswith');
+var fs 		= require('fs'),
+	stylus 	= require('stylus'),
+	argv 	= require('minimist')(process.argv.slice(2));
+
 module.exports = function (babel) {
-  var Plugin = babel.Plugin;
-  var t = babel.types;
+	var Plugin = babel.Plugin,
+		t = babel.types;
 
-  //
-  var argv = require('minimist')(process.argv.slice(2))
-  var path = argv._[0]
-  //
-
-  return new Plugin('stylus', {
-    visitor: {
-      ImportDeclaration: {
-        exit: function(node, parent, scope, file) {
-            if (node && node.source && node.source.value && node.source.type === 'Literal') {
-              if (node.source.value.indexOf('.styl') !== -1) {
-                var fs = require('fs');
-                var stylus = require('stylus')
-                var filename = path + node.source.value
-                var content = fs.readFileSync(filename, 'utf8');
-                var css = stylus(content).render()
-                var id = node.specifiers[0].local.name
-                return t.variableDeclaration('var', [t.variableDeclarator(t.identifier(id), t.literal(css))])
-              }
-            }
-        }
-      }
-    }
-  });
+	return new Plugin('stylus', {
+		visitor: {
+			ImportDeclaration: {
+				exit: function(node, parent, scope, file) {
+					if (node && node.source && node.source.value && node.source.type === 'Literal') {
+						if (node.source.value.endsWith('.styl')) {
+							var filename = argv._[0] + node.source.value,
+								content = fs.readFileSync(filename, 'utf8'),
+								css = stylus(content).render(),
+								id = node.specifiers[0].local.name
+							return t.variableDeclaration('var', [t.variableDeclarator(t.identifier(id), t.literal(css))])
+						}
+					}
+				}
+			}
+		}
+	});
 };
